5d9caa59f7ec096776faab0f60de0060383f1362
Lucas Henrique da Cruz <lucascruzinvest@hotmail.com>
2025-06-13 09:35:38 -0300

    VLD.sh

---
+++ VLD.sh
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+echo.
+echo                                                           ######################################
+echo                                                          #                                      #
+echo                                                          #   v           v  L       DDDDDDD     #
+echo                                                          #    v         v   L       D      D    #
+echo                                                          #     v       v    L       D      D    #
+echo                                                          #      v     v     L       D      D    #
+echo                                                          #       v   v      L       D      D    #
+echo                                                          #        v v       L       D      D    #
+echo                                                          #         v        LLLLLL  DDDDDDD     #
+echo                                                          #                                      #
+echo                                                          #            Delevi Corp               #
+echo                                                           ######################################
+echo.
